4529b5bb00201289fbc0720ef263b1c651684d1a
Mateusz Matyszkkowicz <mateusz.matyszkowicz@boldare.com>
2020-09-07 18:49:05 +0200

    Setup tailwindcss

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SlackClone } from "./slack-clone";
+
+function App() {
+  return (
+    <div className="App">
+      <SlackClone />
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import './assets/index.css';
+import './assets/main.css'
 
 ReactDOM.render(
   <React.StrictMode>
