965f611f7aabb00ed4e908862cc55372e2e928a6
Adam S <azslomian@gmail.com>
2020-09-15 19:43:46 +0200

    landing pge - fixes for button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,5 +2,5 @@ import * as React from "react";
 type ButtonProps = { title: string, color: string, type?: 'primary' | 'white' };
 
 export const Button = ({ title, color = 'text-gray-500', type}: ButtonProps) => (
-    <a className={`inline-block py-1 md:py-4 ${color}`}>{title}</a>
+    <a className={`inline-block py-2 px-4 ${color} ${type === 'primary' ? 'bg-white hover:bg-gray-100 rounded-lg' : ''} `}>{title}</a>
 );
--- src/pages/landing-main-page/landing-page-header.tsx
+++ src/pages/landing-main-page/landing-page-header.tsx
@@ -28,18 +28,8 @@ export const LandingPageHeader = () => (
           </div>
         </div>
         <div className="hidden md:block">
-          <Button title='Login' color='text-gray-500' type='primary'/>
-          <Button title='Sign Up' color='text-gray-700' />
-          {/* <a
-            href="#"
-            className="inline-block py-1 md:py-4 text-gray-500 hover:text-gray-100 mr-6"
-            >Login</a
-          >
-          <a
-            href="#"
-            className="inline-block py-2 px-4 text-gray-700 bg-white hover:bg-gray-100 rounded-lg"
-            >Sign Up</a
-          > */}
+          <Button title='Login' color='text-gray-500' />
+          <Button title='Sign Up' color='text-gray-700' type='primary'/>
         </div>
       </div>
     </div>
