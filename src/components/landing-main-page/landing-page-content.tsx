e65077567f565953eb4be593ddc3abefb42a9f58
Adam S <azslomian@gmail.com>
2020-09-15 17:50:02 +0200

    Landing page created

--- src/components/landing-main-page/landing-page-content.tsx
+++ src/components/landing-main-page/landing-page-content.tsx
@@ -476,21 +476,5 @@ export const LandingPageContent = () => (
         <path fill-opacity="1" d="M0,224L1440,32L1440,320L0,320Z"></path>
       </svg>
     </div>
-
-    <p className="text-center p-4 text-gray-600 pt-10">
-      Created by
-      <a
-        className="border-b text-blue-500"
-        href="https://twitter.com/mithicher"
-        target="_blank"
-        >@mithicher</a
-      >. Inspired by dribble shot
-      <a
-        href="https://dribbble.com/shots/8807920-Quickpay-Hero-section/attachments/1015863"
-        target="_blank"
-        className="border-b text-blue-500"
-        >https://dribbble.com/vadimdrut</a
-      >
-</p>
 </div>
 );
